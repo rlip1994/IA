f9e64f9ba8944587b0b802007e20ea1a74e5ce37
AndreSolis <solisbarrantes@gmail.com>
2017-09-13 18:45:16 -0600

    proyecto con maven y fsm

---
+++ IA-Laberinto/src/main/java/com/mycompany/ia/laberinto/test.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ia.laberinto;
+
+/**
+ *
+ * @author Test
+ */
+public class test {
+  public static void main(String[] args) {
+    System.out.println("hola");
+   
+
+   
+   
+  } //Cierre del main
+}
